9e1f7af5ddbadc532e2b0c3fa67754d03d5b79d5
noahw <noahbweiner@gmail.com>
2017-03-02 14:26:45 -0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// @flow
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
+let value: number = "abcd";
+
+ReactDOM.render(
+  <div className="container">
+    <h1>Hello world!</h1>
+  </div>,
+  document.getElementById('root')
+);
