44b31b7cfdaab44f5485937f27c6514868612db1
noahw <noahbweiner@gmail.com>
2017-03-03 16:55:15 -0800

    move stylesheets to seperate folder

--- src/main/index.js
+++ src/main/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.css';
 
-import './index.css';
+import './stylesheets/bootstrap.css';
+import './stylesheets/font-awesome/css/font-awesome.css';
+import './stylesheets/index.css';
+
 
 import store from './store';
 import App from './components/app';
