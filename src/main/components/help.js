5dc8c93a8df226958a5194290c76a7d870cd547d
noahw <noahbweiner@gmail.com>
2017-03-04 17:14:19 -0800

    deselect note with esc

--- src/main/components/help.js
+++ src/main/components/help.js
@@ -3,9 +3,11 @@ import React from 'react';
 const Help = () => (
   <ul className="help-text">
     <li><kbd>click</kbd> a note to select it</li>
+    <li><kbd>escape</kbd> to deselect a note</li>
     <li><kbd>shift+click</kbd> another note to connect them</li>
-    <li><kbd>double-click</kbd> on the graph to create a new note</li>
+    <li><kbd>double click</kbd> on the graph to create a new note</li>
     <li><kbd>shift+space</kbd> to toggle edit mode</li>
+    <li><kbd>esc</kbd> to toggle edit mode</li>
   </ul>
 );
 
--- src/main/components/toolbar/close-button.js
+++ src/main/components/toolbar/close-button.js
@@ -1,10 +1,38 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
-const CloseButton = ({ onClick }) => (
-  <button className="close" type="button" onClick={onClick}>
-    &times;
-  </button>
-);
+class CloseButton extends Component {
+  constructor(props) {
+    super(props);
+    this.onKeyDown = this.onKeyDown.bind(this);
+  }
+
+  componentWillMount() {
+    document.addEventListener('keydown', this.onKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.onKeyDown);
+  }
+
+  onKeyDown(e) {
+    const { onClick } = this.props;
+
+    if (e.code === 'Escape') {
+      e.preventDefault();
+      onClick.call();
+    }
+  }
+
+  render() {
+    const { onClick } = this.props;
+
+    return (
+      <button className="close" type="button" onClick={onClick}>
+        &times;
+      </button>
+    );
+  }
+}
 
 CloseButton.propTypes = {
   onClick: PropTypes.func
