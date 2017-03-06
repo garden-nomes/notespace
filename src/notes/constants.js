ece43d2768e2de4af307e040e4f047efdec7d06e
noahw <noahbweiner@gmail.com>
2017-03-05 16:46:40 -0800

    polish welcome text

--- src/notes/constants.js
+++ src/notes/constants.js
@@ -2,8 +2,8 @@ export const WELCOME = `# Welcome to notespace
 
 ##### This is a simple little app for creating graphs of connected notes (think like mind maps).
 ***
-To get started, hit "edit" at the top of this page to edit this note or double click somewhere on the graph to create a new note. Shift-click another note to connect them.
+To get started, hit *edit* at the top of this page to edit this note or *double click* on the graph to create a new note. *Shift-click* another note to connect them.
 
-Notes will be saved in your browser's local storage, so don't worry -- you can close this tab and come back to it later if you need a nap.
+Notes will be saved in your browser's local storage, so don't worry -- you can close this tab and come back to it later if you want to take a nap.
 
 Hope it's useful! 👍`;
